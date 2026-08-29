17ec10b3a7aff99f5c4871674049179ae47ab500
Vince Volpone <vincent.volpone@gmail.com>
2026-08-29 11:27:46 -0400

    Slowly working on getting useful data from the FIT files, mostly copied AI's suggestions on data by iterating through the message values and maching via field number for the associated data types

--- src/activity.rs
+++ src/activity.rs
@@ -1,6 +1,6 @@
 use std::path::PathBuf;
 
-use fit::Fit;
+use fit::{Fit, Value};
 
 use serde::{Deserialize, Serialize};
 
@@ -37,22 +37,71 @@ impl Activity {
     fn new(fp: PathBuf) {
         let fit = Fit::new(&fp);
         for m in fit {
-            // dbg!(&m);
             match m.kind {
                 //Save segment info
                 fitsdk::MessageType::SegmentLap => {
                     println!("segment data!");
                     dbg!(&m.values);
                 }
-                fitsdk::MessageType::TimestampCorrelation => {
-                    println!("Time stamp data!");
-                    dbg!(&m.values);
+                //Save time info
+                //field 3 is local time
+                fitsdk::MessageType::Record => {
+                    let mut time: Option<u32> = None; //264
+                    let mut distance_m: Option<u32> = None; //5
+                    let mut speed_kmh: Option<u16> = None; //6
+                    let mut heart_rate: Option<u8> = None; //3  
+                    let mut latitude: Option<i32> = None; //0   
+                    let mut longitude: Option<i32> = None; //1 
+                    let mut altitude_m: Option<u16> = None; //2
+                    let mut slope_pct: Option<f32> = None;
+
+                    for field in m.values {
+                        match field.field_num {
+                            264 => {
+                                if let Value::Time(timestamp) = field.value {
+                                    time = Some(timestamp);
+                                }
+                            }
+                            5 => {
+                                if let Value::U32(distance) = field.value {
+                                    distance_m = Some(distance);
+                                }
+                            }
+                            6 => {
+                                if let Value::U16(speed) = field.value {
+                                    speed_kmh = Some(speed);
+                                }
+                            }
+                            3 => {
+                                if let Value::U8(hr) = field.value {
+                                    heart_rate = Some(hr);
+                                }
+                            }
+
+                            0 => {
+                                if let Value::I32(lat) = field.value {
+                                    latitude = Some(lat);
+                                }
+                            }
+                            1 => {
+                                if let Value::I32(long) = field.value {
+                                    longitude = Some(long);
+                                }
+                            }
+                            2 => {
+                                if let Value::U16(alt) = field.value {
+                                    altitude_m = Some(alt);
+                                }
+                            }
+                            _ => {}
+                        }
+                    }
                 }
                 _ => continue,
             }
         }
-        //eventually return something
     }
+    //eventually return something
 }
 
 mod d_test {
@@ -65,7 +114,9 @@ mod d_test {
 
     #[test]
     fn test_fit() {
-        Activity::new(PathBuf::from("./Data/TestSeg.fit"));
+        Activity::new(PathBuf::from(
+            "./Data/thatonematrixman@gmail.com_470194221701.fit",
+        ));
         assert_eq!(false, true);
     }
 }
