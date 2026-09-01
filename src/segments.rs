8dba9e7d0ec5e4f771f70132dd5e78c3ef43b2c8
Vincent Volpone <vincent.volpone@gmail.com>
2026-08-31 22:08:49 -0400

    Added nalgebra to use the vector objects they have in order to do some basic vector math to be able to start drawing up gates I think I'm moving away from trying to categorize turns as they very greatly. for now I'm going to start with split gates at different intervals of measurement 5 20 60 (I think) I'll eventually toss back in elevation stuff to know if I'm slow going up climbs and what not

--- src/segments.rs
+++ src/segments.rs
@@ -1,25 +1,63 @@
 use std::{fs, path::PathBuf};
 
 use crate::{BIN_SAVE_LOC, activity::Activity};
+use nalgebra::{Point2, Vector, Vector2};
 use rkyv::{Archive, Deserialize, Serialize, access, deserialize, rancor};
 
+//Starting with gaps -- basically split gaps at different intervals as I think categorizing
+// turns may be a difficult starting point not worth digging into right at the start
+// I do want to add uphills and downhills later
 #[derive(Debug, Archive, Serialize, Deserialize)]
 #[rkyv(compare(PartialEq), derive(Debug))]
 struct Segment {
     name: String,
-    turns: Vec<((f32, f32), (f32, f32))>, //turns marked by (start long,lat , end long,lat)
-    uphills: Vec<((f32, f32), (f32, f32))>, //turns marked by (start long,lat , end long,lat)
-    downhills: Vec<((f32, f32), (f32, f32))>, //turns marked by (start long,lat , end long,lat)
+    small_gap: Vec<gate>, //every 5 readings on ref
+    med_gap: Vec<gate>,   //every 20 readings on ref
+    large_gap: Vec<gate>, //every 60 readings on ref
+                          // uphills: Vec<(gate,gate)>,
+                          // downhills: Vec<(gate,gate)>,
 }
 
 // using gate as a method of capturing when the rider goes through a part on the path.
 // a line from left to right pivot show the gate itself and the in/out ref are to be able
 // to compare whether the rider is going through the gate in the right direction
+#[derive(Debug, Archive, Serialize, Deserialize)]
+#[rkyv(compare(PartialEq), derive(Debug))]
 struct gate {
     left_pivot: (f32, f32),
     right_pivot: (f32, f32),
     inside_ref: (f32, f32),
     outside_ref: (f32, f32),
+}
+
+impl gate {
+    fn new(points: [(f32, f32); 3], length: f32) -> Option<gate> {
+        //check the data_analytics image under gates (2.1) for what the goal is here
+        let point_a = Point2::new(points[0].0, points[0].1);
+        let point_b = Point2::new(points[1].0, points[1].1);
+        let point_c = Point2::new(points[2].0, points[2].1);
+
+        //vectorize the two lines then grab the normalized perpindicular line
+        let vec_ab: Vector2<f32> = point_b - point_a;
+        let vec_bc: Vector2<f32> = point_c - point_b;
+        let vec_ab_perp: Vector2<f32> = Vector2::new(-vec_ab.y, vec_ab.x);
+        let vec_bc_perp: Vector2<f32> = Vector2::new(-vec_bc.y, vec_bc.x);
+        let norm_vec_ab_perp = vec_ab_perp.normalize() * length / 2.0;
+        let norm_vec_bc_perp = vec_bc_perp.normalize() * length / 2.0;
+
+        //Form those two gates based on the two vectors than take the midpoints
+        // to form the new finalized gate
+        let ab_gate = (point_a - norm_vec_ab_perp, point_b + norm_vec_ab_perp);
+        let bc_gate = (point_b - norm_vec_bc_perp, point_c + norm_vec_bc_perp);
+        let full_gate = Vector2::new(
+            (ab_gate.0 + (bc_gate.0 - ab_gate.0) / 2.0),
+            (ab_gate.1 + (bc_gate.1 - ab_gate.1) / 2.0),
+        );
+
+        //Normalize the full gate and apply the length to it
+
+        todo!();
+    }
 }
 
 impl Segment {
